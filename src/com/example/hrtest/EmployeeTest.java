597c210b0759df5e1cff7183d7fad3c306312484
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-24 11:50:24 +0530

    Update EmployeeTest.java

--- src/com/example/hrtest/EmployeeTest.java
+++ src/com/example/hrtest/EmployeeTest.java
@@ -3,10 +3,16 @@ package com.example.hrtest;
 import com.example.hr.Employee;
 
 public class EmployeeTest {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Employee employee = new Employee("John Daniel", 53201, 85000.0);
         employee.printName();
         employee.printID();
         employee.printSalary();
     }
 }
+---------------------------------------------------------------------------------------------------
+OUTPUT:
+    
+Employee name: John Daniel
+Employee ID: 53201
+Employee salary: 85000.0
